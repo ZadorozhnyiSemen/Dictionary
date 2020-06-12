92c6af0b7d7c3eacdf2911369504375ad9cfd403
szadorozhnyi <szadorozhnyi@freetech.org>
2020-06-12 15:08:55 +0300

    Add model for search

---
+++ app/src/main/java/com/semyon/dictionary/model/PreviewWord.kt
@@ -0,0 +1,23 @@
+package com.semyon.dictionary.model
+
+data class PreviewWord(
+    val id: Int,
+    val text: String,
+    val meanings: List<WordMeaning>
+
+)
+
+data class WordMeaning(
+    val id: Int,
+    val partOfSpeechCode: String,
+    val translation: WordTranslation,
+    val previewUrl: String,
+    val imageUrl: String,
+    val transcription: String,
+    val soundUrl: String
+)
+
+data class WordTranslation(
+    val text: String,
+    val note: String
+)
