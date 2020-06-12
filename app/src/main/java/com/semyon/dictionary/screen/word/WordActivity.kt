5e9aa257fda9a141d58cfdb0559e6c7ed80b87cf
szadorozhnyi <szadorozhnyi@freetech.org>
2020-06-12 21:48:52 +0300

    Add UI for description and transcription

--- app/src/main/java/com/semyon/dictionary/screen/word/WordActivity.kt
+++ app/src/main/java/com/semyon/dictionary/screen/word/WordActivity.kt
@@ -1,8 +1,12 @@
 package com.semyon.dictionary.screen.word
 
+import android.annotation.SuppressLint
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
 import com.semyon.dictionary.R
@@ -16,19 +20,46 @@ const val WORD_ID_KEY = "word_id_key"
 class WordActivity : AppCompatActivity() {
 
     private val wordDetailsViewModel: WordDetailViewModel by viewModels()
+    private var mediaPlayer: MediaPlayer? = MediaPlayer().apply {
+        setAudioAttributes(AudioAttributes.Builder()
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .build()
+        )
+    }
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_word)
 
-        wordDetailsViewModel.word.observe(this, Observer {
+        wordDetailsViewModel.word.observe(this, Observer { meaning ->
             Glide.with(this)
-                .load("https:${it.images.first().url}")
+                .load("https:${meaning.images.first().url}")
                 .centerCrop()
                 .into(word_image)
+            word_origin.text = meaning.text
+            word_translation.text = meaning.translation.text
+            word_transcription.text = "/${meaning.transcription}/"
+            word_description.text = meaning.definition.text
+            play_translation.setOnClickListener {
+                mediaPlayer?.apply {
+                    reset()
+                    setDataSource(this@WordActivity, "https:${meaning.soundUrl}".toUri())
+                    prepareAsync()
+                    setOnPreparedListener {
+                        it.start()
+                    }
+                }
+            }
         })
 
         val wordId = intent.getIntExtra(WORD_ID_KEY, -1)
         wordDetailsViewModel.loadWordInformation(wordId)
+    }
+
+    override fun onStop() {
+        mediaPlayer?.release()
+        mediaPlayer = null
+        super.onStop()
     }
 }
