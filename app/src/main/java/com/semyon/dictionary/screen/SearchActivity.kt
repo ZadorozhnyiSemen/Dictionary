417c724330cbc071c7b0728d31a5f74d88d90213
szadorozhnyi <szadorozhnyi@freetech.org>
2020-06-12 12:47:11 +0300

    Add screens

--- app/src/main/java/com/semyon/dictionary/screen/SearchActivity.kt
+++ app/src/main/java/com/semyon/dictionary/screen/SearchActivity.kt
@@ -1,12 +1,13 @@
-package com.semyon.dictionary
+package com.semyon.dictionary.screen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.semyon.dictionary.R
 
-class MainActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_search)
     }
 }
---
+++ app/src/main/java/com/semyon/dictionary/screen/WordActivity.kt
@@ -0,0 +1,13 @@
+package com.semyon.dictionary.screen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.semyon.dictionary.R
+
+class WordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_word)
+    }
+}
